6cb166ac2324567d845c7fe452ed1992b32f91e9
Anatolii Kravchenko <tol890@ukr.net>
2022-12-01 19:10:40 +0200

    Finished (Logic needs attention)

--- words.js
+++ words.js
@@ -1,12 +1,8 @@
 'use strict';
-const minX = 0;
-const minY = 0;
-const minZ = 0;
-const maxX = 63;
-const maxY = 63;
-const maxZ = 63;
 
-
+const minNum = 0;
+const maxNum = 63;
+const varCount = 3;
 
 function getOne()
             {
@@ -15,8 +11,8 @@ function getOne()
                 const btnClean = document.getElementById("btn_clean");
                 const defaultSize = document.querySelector('#populationSize');
                 const defaultGenerations = document.querySelector('#defaultGenerations');
-
-
+                let interationCounter = 0;
+                
                 // Events
                 form.addEventListener('submit', formSubmit);
                 btnClean.addEventListener('click', formClean);
@@ -28,40 +24,37 @@ function getOne()
                     outputWindow.textContent += `Кількість поколінь: ${defaultGenerations.value};\n`
                     outputWindow.textContent += "Вивід роботи алгоритму:\n";
                     outputWindow.textContent += "....\n";
-                    // Src: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
                     function random(min, max) {
                         min = Math.ceil(min);
                         max = Math.floor(max);
-                    
                         // The maximum is exclusive and the minimum is inclusive
                         return Math.floor(Math.random() * (max - min)) + min;
                     }
                     
-                    function generateLetter() {
-                        const code = random(97, 123); // ASCII char codes
-                        return String.fromCharCode(code);
+                    function generateNumber() {
+                        const number = random(minNum, maxNum);
+                        return number;
+                    }
+
+                    function fintessFunction(x, y, z) {
+                        const fitnessF =  4 / x + x^2 / y + y^2 / z + z^2 / 5
+                        return fitnessF;
                     }
                     
                     class Member {
                         constructor(target) {
                             this.target = target;
                             this.keys = [];
-                        
-                            for (let i = 0; i < target.length; i += 1) {
-                                this.keys[i] = generateLetter();
+                            
+                            for (let i = 0; i < varCount; i += 1) {
+                                this.keys[i] = generateNumber();
                             }
+                            this.F = fintessFunction(this.keys[0], this.keys[1], this.keys[2]);
                         }
                     
                         fitness() {
-                            let matches = 0;
-                        
-                            for (let i = 0; i < this.keys.length; i += 1) {
-                                if (this.keys[i] === this.target[i]) {
-                                    matches += 1;
-                                }
-                            }
-                    
-                            return matches / this.target.length;
+                            return this.F;
                         }
                     
                         crossover(partner) {
@@ -82,10 +75,8 @@ function getOne()
                     
                         mutate(mutationRate) {
                             for (let i = 0; i < this.keys.length; i += 1) {
-                                // If below predefined mutation rate,
-                                // generate a new random letter on this position.
                                 if (Math.random() < mutationRate) {
-                                    this.keys[i] = generateLetter();
+                                    this.keys[i] = generateNumber();
                                 }
                             }
                         }
@@ -96,39 +87,49 @@ function getOne()
                             size = size || 1;
                             this.members = [];
                             this.mutationRate = mutationRate;
+                            this.fitnessAllMembers = [];
                         
                             for (let i = 0; i < size; i += 1) {
                                 this.members.push(new Member(target));
                             }
                             // Print population
                             outputWindow.textContent += "Популяція: \n";
-                            outputWindow.textContent += "X, Y, Z, F\n";
+                            outputWindow.textContent += "[X,Y,Z] : F \n";
                             for (let i of this.members) {
-                                outputWindow.textContent += `${i.keys} \n`;
+                                outputWindow.textContent += `[${i.keys}] : ${i.F} \n`;
                             }
+                            for (let member of this.members) {
+                                this.fitnessAllMembers.push(member.F)
+                            }
+                            
                         }
                     
                         evolve(generations) {
                             for (let i = 0; i < generations; i += 1) {
                                 const pool = this._selectMembersForMating();
                                 this._reproduce(pool);
+                                interationCounter++;
                             }
                         }
                     
                         _selectMembersForMating() {
                             const matingPool = [];
-                        
+                            const midpoint = this.fitnessAllMembers[this.fitnessAllMembers.length / 2]
                             this.members.forEach((m) => {
-                                // The fitter he/she is, the more often will be present in the mating pool
-                                // i.e. increasing the chances of selection
-                                // If fitness == 0, add just one member
-                                const f = Math.floor(m.fitness() * 100) || 1;
-                        
-                                for (let i = 0; i < f; i += 1) {
+                                if (m.F >= midpoint) {
                                     matingPool.push(m);
                                 }
+                                
                             });
-                        
+
+                            matingPool.forEach((matingMember) => {
+                                console.log(`matingMember: ${matingMember.F}`)
+                            })
+                            this.fitnessAllMembers.sort(function(a, b) {
+                                return a - b;
+                            });
+                            let mostFitPool = [];
+
                             return matingPool;
                         }
                     
@@ -150,22 +151,25 @@ function getOne()
                     }
                     
                     // Init function
-                    function generate(populationSize, target, mutationRate, generations) {
+                    function generate(populationSize, mutationRate, generations, target=20) {
                         // Create a population and evolve for N generations
                         const population = new Population(populationSize, target, mutationRate);
                         population.evolve(generations);
-                    
+                        // Termination criteria
+                        if (interationCounter > defaultGenerations.value) {
+                            console.log('finished')
+                        }
+
                         // Get the typed words from all members and find if someone was able to type the target
                         const membersKeys = population.members.map((m) => m.keys.join(''));
                         const perfectCandidatesNum = membersKeys.filter((w) => w === target);
                     
                         // Print the results
-                        console.log(membersKeys);
                         outputWindow.textContent += (`${membersKeys}\n`);
                         outputWindow.textContent += `${perfectCandidatesNum ? perfectCandidatesNum.length : 0} member(s) typed "${target}"\n`;
                     }
                     
-                    generate(20, 'hit', 0.05, 20);
+                    generate(defaultSize.value, 0.05, defaultGenerations.value);
                     outputWindow.textContent += "--- Кінець виконання алгоритму ---\n"
                     outputWindow.textContent += "***************************************\n"
 
